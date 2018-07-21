e5b5bce490b771fea2da93c45772488eba783048
tomato <nomu.tomato.zyusu@gmail.com>
2018-07-21 16:01:20 +0900

    add a test

--- testForest/forest/model/TreeTest.java
+++ testForest/forest/model/TreeTest.java
@@ -1,6 +1,10 @@
 package forest.model;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +12,21 @@ import static org.junit.Assert.*;
  * Created by Tomato on 2018/07/21.
  */
 public class TreeTest {
+    @Before
+    public void setUp() throws Exception {
+
+        TreeParser aTreeParser = new TreeParser();
+        List<Node<NodeData>> aNodeList = aTreeParser.parseText("test");
+        List<Tree<NodeData>> aTreeList = new ArrayList<Tree<NodeData>>();
+
+        aNodeList.stream().filter(Node::isRoot).forEach((aNode) -> {
+                Tree<NodeData> aTree = new Tree<NodeData>(aNode);
+                // aTree.sortLevelOrder();
+                aTreeList.add(aTree);
+            }
+        });
+    }
+
     @Test
     public void getNodes() throws Exception {
     }
